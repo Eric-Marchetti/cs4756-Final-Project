25b5ebb96f1f354128d02d348c27e4855fd886a5
Eric-Marchetti <44625126+Eric-Marchetti@users.noreply.github.com>
2024-12-06 15:53:08 -0500

    Update initial.py

--- initial.py
+++ initial.py
@@ -1,3 +1,4 @@
+from random import random
 risk_map = {
     "North America": {
         "Alaska": {"neighbors": ["Northwest Territory", "Alberta", "Kamchatka"], "continent": "North America"},
@@ -54,3 +55,156 @@ risk_map = {
         "Eastern Australia": {"neighbors": ["New Guinea", "Western Australia"], "continent": "Australia"},
     }
 }
+continent_bonus = {
+            "North America": 5,
+            "South America": 2,
+            "Europe": 5,
+            "Africa": 3,
+            "Asia": 7,
+            "Australia": 2
+        }
+
+class Player:
+    def __init__(self, name, color):
+        self.color = color
+        self.territories = {} # {territory: units}
+        self.cards = []
+        self.units = 0
+
+    def add_territory(self, territory, units):
+        self.territories[territory] = units
+
+    def remove_territory(self, territory):
+        del self.territories[territory]
+
+    def add_units(self, territory, units):
+        self.units += units
+        self.territories[territory] += units
+
+    def remove_units(self, territory, units):
+        self.units -= units
+        self.territories[territory] -= units
+
+    def get_units_in_territory(self, territory):
+        return self.territories[territory]
+    
+    def getCards(self):
+        return self.cards
+
+class Card:
+    def __init__(self, territory, unit_type):
+        self.territory = territory
+        self.unit_type = unit_type
+
+class Game:
+    def __init__(self, players):
+        self.players = players
+        self.current_player = 0
+        self.turn = 0
+        #self.turn_phase = "reinforce"
+        self.num_trade_ins = 0
+
+    def initialize_map(self):
+        for continent in risk_map:
+            keys_shuffled = random.shuffle(list(risk_map[continent].keys()))
+            for key in keys_shuffled:
+                territory = risk_map[continent][key]
+                self.players[self.current_player].add_territory(territory, 1)
+                self.players[self.current_player].units += 1
+                self.current_player = (self.current_player + 1) % len(self.players)
+
+    def next_player(self):
+        self.current_player = (self.current_player + 1) % len(self.players)
+        if self.current_player == 0:
+            self.turn += 1
+
+    def reinforce(self, territory, units):
+        self.players[self.current_player].add_units(territory, units)
+        self.reinforcements -= units
+
+    def attack(self, attacking_player, attacking_territory, defending_player, defending_territory):
+        attacker_rolls = min(attacking_player.get_units_in_territory(attacking_territory), 3)
+        defender_rolls = min(defending_player.get_units_in_territory(defending_territory), 2)
+
+        attacker_dice = [random.randint(1, 6) for i in range(attacker_rolls)].sort(reverse=True)
+        defender_dice = [random.randint(1, 6) for i in range(defender_rolls)].sort(reverse=True)
+
+        for i in range(min(attacker_rolls, defender_rolls)):
+            if attacker_dice[i] > defender_dice[i]:
+                defending_player.remove_units(defending_territory, 1)
+            else:
+                attacking_player.remove_units(attacking_territory, 1)
+
+    def fortify(self, player, origin_territory, destination_territory, units):
+        player.remove_units(origin_territory, units)
+        player.add_units(destination_territory, units)  
+
+    def turn_in_cards(self, player, cards):
+        if len(cards) != 3:
+            print("Invalid trade: must trade exactly 3 cards.")
+            return 0
+
+        if not all(card in player.getCards for card in cards):
+            print("Invalid trade: player does not own all these cards.")
+            return 0
+        
+        if len(set(cards)) == 1 or len(set(cards)) == 3:
+            for card in cards:
+                player.cards.remove(card)
+            
+        if self.num_trade_ins < 5:
+            num_troops = 4 + 2 * self.num_trade_ins
+        elif self.num_trade_ins == 6:
+            num_troops = 15
+        else:
+            num_troops = 5 * self.num_trade_ins - 15
+
+        self.num_trade_ins += 1
+        return num_troops
+
+
+    def check_win(self):
+        if len(self.players) == 1:
+            return True
+    
+    def get_winner(self):
+        return self.players[0]
+
+    def check_elimination(self):
+        for player in self.players:
+            if len(player.territories) == 0:
+                print(player.name + " has been eliminated.")
+                self.players.remove(player)
+                
+    def check_continent(self, continent):
+        for player in self.players:
+            if all(territory in player.territories for territory in risk_map[continent]):
+                return player
+    
+    def get_continent_bonus(self, continent):
+        return continent_bonus[continent]
+
+    def check_elimination_bonus(self, killing_player, eliminated_player):
+        if eliminated_player.territories: # if they still have land they not eliminated
+            return
+        eliminated_player_cards = eliminated_player.getCards()
+        if len(eliminated_player_cards) > 0:
+            killing_player.cards += eliminated_player_cards
+
+
+
+def main():
+    playerA = Player("Player A", "red")
+    playerB = Player("Player B", "blue") 
+    game = Game([playerA, playerB])
+    game.initialize_map()
+    while not game.check_win():
+        #place troops
+
+        #attack
+
+        # fortify
+
+        game.next_player()
+    
+    print(game.get_winner().name + " wins!")
